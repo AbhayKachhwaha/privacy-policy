32948f880f1bc5a3193a29d868418a553ef7b112
Abhay <abhay.kachhwaha09@gmail.com>
2022-11-22 21:53:43 -0500

    Added privacy policy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,320 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Ebony Market privacy policy page.</h1>
+
+      <h2>Privacy Statement Overview</h2>
+      <p>
+        This policy tells you how we use personal information collected at Ebony
+        Market (“we,” “us,” “our,” etc.), from our users (“you,” “your,” etc.)
+        of Ebony Market App and any related website(s), and any
+        subsequently acquired and/or developed applications, to the extent
+        applicable (collectively, the “Site”). This Privacy Policy applies only
+        to the Site. It does not apply to any third-party services linked to our
+        site, to the extent applicable, or offline activities related to our
+        services. We urge you to read this Privacy Policy in its entirety so you
+        will understand all of the practices, principles and procedures we
+        follow relating to your online privacy. In this Privacy Policy, we will
+        inform you about the type of information that we collect about you on
+        this Site, how it is collected, what it will be used for, and to whom it
+        will be shared in the event that there is a need for sharing such
+        information. This policy will also tell you how to limit our use of the
+        personal information that you will be providing to us overtime and
+        covers how we treat your personal identifiable information that we
+        gather from you whenever you access our website and/or make use of our
+        services. This Privacy Policy is incorporated into our Terms and
+        Conditions of Use. We may from time to time need to update this Privacy
+        Policy in order to keep pace with changes in the Site, our business and
+        the law, however, we always maintain our commitment to respect your
+        privacy. We will notify you of any revisions that we make to this our
+        Privacy Policy, along with their effective date, in an easy to find area
+        of the Site, and we recommend that you periodically check back here to
+        stay informed of any changes and see how your continued usage of our
+        website shall be guided by. This Privacy Policy does not apply to
+        information collected by us offline or through any other means,
+        including on any other website operated by us or any third party
+        (including our affiliates and subsidiaries); or any third party
+        (including our affiliates and subsidiaries), including through any
+        application or content (including advertising) that may link to or be
+        accessible from or on the Site. IF YOU DO NOT AGREE WITH ANY OF THE
+        FOLLOWING PRIVACY POLICY TERMS, PLEASE DO NOT ACCESS THE SITE OR
+        DISCONTINUE YOUR USAGE OF THE SITE IF YOU ALREADY STARTED USING THE
+        SITE.
+      </p>
+      <h2>About Us (“Ebony Market LLC.”)?</h2>
+      <p>
+        Ebony Market app makes it easy for you to find and support black
+        businesses locally.
+      </p>
+      <h3>Privacy Notice</h3>
+      <p>
+        This Privacy Notice describes Ebony Market’s collection, use, and
+        disclosure of the information You provide to Us through Your use of the
+        Site or Service, as well as the rights You have with respect to Your
+        Personal Data. For any questions regarding this Privacy Notice please
+        contact Us
+      </p>
+      <h3>WHO PROCESSES MY PERSONAL INFORMATION?</h3>
+      <p>
+        Ebony Market is the sole controller of the data that we gather from you
+        and we are responsible for your personal information or data
+        (collectively referred to as "Ebony Market", "we", "us" or "our" in this
+        privacy policy).
+      </p>
+      <h3>WHAT DOES THIS PRIVACY POLICY COVER?</h3>
+      <p>
+        This Privacy Policy covers EBONY MARKET’ treatment of your personally
+        identifiable information ("Personal Information") that EBONY MARKET
+        collects when you are accessing any of EBONY MARKET' Service and when
+        you use our services.
+      </p>
+      <p>What Information We Collect and How Do We Use It?</p>
+      <h3>INFORMATION WE COLLECT</h3>
+      <p>
+        We believe your Information is no one else's. Your privacy is important
+        to you and to us. So we'll ensure at all time that we protect the
+        information you share with us. To protect your privacy, EBONY MARKET
+        follows different principles in accordance with world-wide practices for
+        customer privacy and data protection and we won’t sell or give away your
+        name, phone number, and email address, or any other information to
+        anyone.
+      </p>
+      <p>
+        Personal information means all information that relates to an identified
+        individual or to an identifiable individual. For example, your full
+        name, email address, and phone number. Personal information is also
+        referred to as information about you. The information we collect from
+        our users enables us to personalize and improve our services and at the
+        same time, to allow our users to set up a meeting with us. We may
+        collect, store, make use of and transfer different kinds of personal
+        data about you. We may collect Personal Data from You, such as Your
+        first and last name, location, phone number, gender, e-mail and mailing
+        addresses, professional title, company name, employment history,
+        education history, personal summary, and password when You create an
+        account to log in to Our network (“Account”).
+      </p>
+      <h3>INFORMATION GATHERED AUTOMATICALLY</h3>
+      <p>
+        We and any of our third-party partners may receive and store certain
+        types of information from you whenever you interact with our services.
+        EBONY MARKET automatically receives and records information on our
+        server logs from your browser including your IP address, cookie
+        information, and the page you requested. These third-party partners also
+        may collect information about your online usage activities over time and
+        on other websites or mobile applications.
+      </p>
+      <p>
+        Generally, our service automatically collects usage information, such as
+        the numbers and frequency of visitors to our site and its components
+        that indicates how frequently people use our platform. EBONY MARKET only
+        uses this data in aggregate form, that is, as a statistical measure, and
+        not in a manner that would identify you personally. This type of
+        aggregate data enables us to figure out how often customers use parts of
+        the Service or services so that we can make the Service appealing to as
+        many customers as possible, and improve those services.
+      </p>
+      <p>
+        As part of this use of information, we may provide aggregate information
+        to our partners about how our users, collectively, use our site. We
+        share this type of statistical data so that our partners also understand
+        how often people use their services and our Service, so that they, too,
+        may provide you with an optimal online experience. Again, EBONY MARKET
+        never discloses aggregate information to a partner in a manner that
+        would identify you personally.
+      </p>
+      <p>
+        We do not in any way gather Special Categories of Personal Data about
+        you (this includes details about your ethnicity or race, philosophical
+        or religious beliefs, political opinions, sex life, sexual orientation,
+        trade union membership, information about your health and genetic and
+        biometric data). We do not collect any information about criminal
+        convictions and offences.
+      </p>
+      <h3>HOW WE USE YOUR PERSONAL INFORMATION?</h3>
+      <p>
+        We collect this personal information in a bid to provide you with a
+        customized and top notch experience and, to communicate with you and
+        respond to your request or application. However, if you get in touch
+        with us (for example, through contact forms on our Sites) we process
+        information about you to communicate with you about your interests on
+        becoming a member of our team
+      </p>
+      <h3>HOW SECURED IS MY PERSONAL INFORMATION?</h3>
+      <p>
+        At EBONY MARKET we have implemented appropriate measures to protect your
+        personal information against any form of alteration, unauthorized access
+        or disclosure as well as all other forms of unlawful or unauthorized
+        processing (including, but not limited to, unnecessary collection) or
+        further processing. In addition, we limit access to your personal data
+        to those employees, agents, contractors and other third parties who have
+        a business need to know. They will only process your personal data on
+        our instructions and they are subject to a duty of confidentiality. We
+        have put in place procedures to deal with any suspected personal data
+        breach and will notify you and any applicable regulator of a breach
+        where we are legally required to do so.
+      </p>
+      <p>
+        Unfortunately, the sending of information via the internet is not
+        totally secure and on occasion such information can be intercepted. We
+        cannot guarantee the security of data that you choose to send us
+        electronically, sending such information is entirely at your own risk.
+      </p>
+      <h3>HOW LONG WE RETAIN YOUR INFORMATION</h3>
+      <p>
+        We will retain personal information We collect from You for the period
+        allowable by law and where We have a justifiable business need to do so,
+        unless a longer period is required or permitted by law. You can request
+        deletion of Your Personal Data at any time and We will consider Your
+        request in accordance with applicable law.
+      </p>
+      <h3>EMAIL COMMUNICATIONS</h3>
+      <p>
+        We are committed to keeping your e-mail address confidential. We do not
+        sell, rent, or lease to third parties, and will not disclose your email
+        address to any third parties except as allowed in the section titled
+        Disclosure of Your Information. We will maintain the information you
+        send via e-mail in accordance with applicable federal law.
+      </p>
+      <p>
+        In compliance with the CAN-SPAM Act, all e-mails sent from our
+        organization will clearly state who the e-mail is from and provide clear
+        information on how to contact the sender. In addition, all e-mail
+        messages will also contain concise information on how to remove yourself
+        from our mailing list so that you receive no further e-mail
+        communication from us.
+      </p>
+      <p>
+        Our emails provide users the opportunity to opt-out of receiving
+        communications from us by reading the unsubscribe instructions located
+        at the bottom of any e-mail they receive from us at any time. Users who
+        no longer wish to receive our newsletter or promotional materials may
+        opt-out of receiving these communications by clicking on the unsubscribe
+        link in the e-mail.
+      </p>
+      <h3>THIRD PARTY LINKS</h3>
+      <p>
+        In the event that we include links to third parties on this website.
+        It’s better be safe and review their privacy policy before sending them
+        any personal data. Our website may include links to third-party
+        websites, plug-ins and applications. Clicking on those links or enabling
+        those connections may allow third parties to collect or share data about
+        you. We do not control these third-party websites and are not
+        responsible for their privacy statements. When you leave our website, we
+        encourage you to read the privacy policy/notice of every website you
+        visit.
+      </p>
+      <h3>DATA RETENTION</h3>
+      <p>
+        We will only retain your personal data for as long as necessary to
+        fulfil the purposes we collected it for, including for the purposes of
+        satisfying any legal, accounting, or reporting requirements.
+      </p>
+      <p>
+        To determine the appropriate retention period for personal data, we
+        consider the amount, nature, and sensitivity of the personal data, the
+        potential risk of harm from unauthorized use or disclosure of your
+        personal data, the purposes for which we process your personal data and
+        whether we can achieve those purposes through other means, and the
+        applicable legal requirements.
+      </p>
+      <h3>PRIVACY POLICY</h3>
+      <p>
+        This Privacy Policy describes in further detail Ebony Market’s
+        collection, processing, and treatment of Your information, as well as
+        the rights You have with respect to such information. This Privacy
+        Policy will provide You with further technical information regarding the
+        processing of Your Personal Data described in the Privacy Notice.
+      </p>
+      <p>
+        If You have any questions or concerns or complaints about this Privacy
+        Policy or Our data collection or processing practices, or if You want to
+        report any security violations to us, please contact Us.
+      </p>
+      <h3>ACCESS TO YOUR PROVIDED INFORMATION</h3>
+      <p>
+        You can opt out from receiving promotional or advertising emails from us
+        by clicking unsubscribe in any of our emails. Please keep in mind that
+        if you unsubscribe from promotional or advertising emails, we may still
+        contact you in response to your requests and we may still send
+        administrative emails, such as those containing information about our
+        services.
+      </p>
+      <p>
+        At any time, you may choose whether or not to provide or disclose
+        Personal Information. However, not providing necessary Personal
+        Information may result in limited access of our services.
+      </p>
+      <p>
+        Your browser or mobile device may give you the ability to control
+        cookies or other types of settings that provide us with information,
+        such as your location. We do not have control over these settings and if
+        you want to make changes about what information is being transmitted,
+        you should refer to information provided by your browser, device, or
+        application.
+      </p>
+      <h3>WHAT ARE YOUR RESPONSIBILITIES?</h3>
+      <p>
+        You are responsible for the personal information that you provide or
+        make available to Us, and you must ensure it is relevant, truthful,
+        accurate and not misleading in any way. You must ensure that the
+        information provided does not contain material that is, defamatory, or
+        infringing on any rights of any third party; does not contain malicious
+        code; and is not otherwise legally actionable. Further, if you provide
+        any personal information concerning any other person, such as
+        individuals you provide as references, you are responsible for providing
+        any notices and obtaining any consents necessary for Oracle to collect
+        and use that information as described in this Privacy Policy.
+      </p>
+      <p>
+        Please note that if you do not want to receive legal notices from us,
+        such as this Privacy Policy, those legal notices will still govern your
+        use of the Our Service, and you are responsible for reviewing such legal
+        notices for changes.
+      </p>
+      <h3>USER CONSENT</h3>
+      <p>
+        By submitting Personal Data through Our Site or Service, you agree to
+        the terms of this Privacy Policy, as well as Our Privacy Notice and the
+        Terms of Service, and You expressly consent to the collection, use and
+        disclosure of Your Personal Data in accordance with the Privacy Policy,
+        Privacy Notice, and Terms of Service. If You do not agree, you should
+        discontinue use of the Site and Service immediately.
+      </p>
+      <h3>DISCLOSURES</h3>
+      <p>
+        Regardless of any choices You make regarding Your Personal Data (as
+        described below), Ebony Market may disclose Personal Data if it believes
+        in good faith that such disclosure is necessary (a) in connection with
+        any legal investigation; (b) to comply with relevant laws or to respond
+        to subpoenas or warrants served on Ebony Market; (c) to protect or
+        defend the rights or property of Ebony Market or users of the Site or
+        Service; and/or (d) to investigate or assist in preventing any violation
+        or potential violation of the law, this Privacy Policy, or Our Terms of
+        Use.
+      </p>
+      <h3>CHILDREN POLICY</h3>
+      <p>
+        We do not intentionally gather Personal Data from visitors who are under
+        the age of 13. If a child under 13 submits Personal Data to Ebony Market
+        and We learn that the Personal Data is the information of a child under
+        13, We will attempt to delete the information as soon as possible. If
+        You believe that We might have any Personal Data from a child under 13,
+        please contact Us
+      </p>
+      <h3>CHANGES TO OUR PRIVACY POLICY</h3>
+      <p>
+        We reserve the sole right to make changes to this policy at any point in
+        time, we therefore advice that you visit this page often. You will
+        always find the current version on this page. If an update reduces your
+        rights, you will be notified upon your continued usage of the website on
+        or after the latest update
+      </p>
+      <h3>CONTACT</h3>
+      <p>
+        If you have any questions or concerns regarding privacy at the EBONY
+        MARKET app, please send us a detailed message to ebonymarkett.contact@gmail.com We will make every
+        effort to resolve your concerns
+      </p>
     </div>
   );
 }
